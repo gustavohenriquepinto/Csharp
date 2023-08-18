fbec9102c04de40c1b591d1431fd11d72dd6135a
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-18 08:30:56 -0300

    Initial commit

---
+++ projeto01/ConsoleApp.NET/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Olá, Mundo!");
+        }
+    }
+}
