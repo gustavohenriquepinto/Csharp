32c6757b1cfb4de99cc8770cea8820ef55c671ba
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-18 09:18:43 -0300

    Comentários e Regions

---
+++ projeto02/MyConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Isso é um boloco de comentário. Tudo que for
+           escrito será ignorado durante a compoilação
+           Lorem ipsum...
+            */
+
+            #region Para se apresentar
+
+            Console.WriteLine("Olá, Mundo1");
+            Console.WriteLine("Olá, Mundo2");
+            Console.WriteLine("Olá, Mundo3");
+           Console.WriteLine("Olá, Mundo4");
+           Console.WriteLine("Olá, Mundo5");
+
+            #endregion
+
+            #region Para se despedir
+
+            Console.WriteLine("Tchau, Mundo1");
+            Console.WriteLine("Tchau, Mundo2");
+            Console.WriteLine("Tchau, Mundo3");
+            Console.WriteLine("Tchau, Mundo4");
+            Console.WriteLine("Tchau, Mundo5");
+
+            #endregion
+        }
+    }
+}
