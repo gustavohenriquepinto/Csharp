596f762f00f4c12822acbb6f1cef6fbeef4d0e31
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-20 20:24:02 -0300

    Conversão

---
+++ Conversão de Variáveis/projeto04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Conversão Implícita
+            //byte num1 = 100;
+            //ushort num2 = num1; //O tipo ushort abrange todos os valores do tipo byte
+
+            //float num3 = -4.8f;
+            //double num4 = num3;
+
+            //char let1 = 'C';
+            //int let2 = let1;
+
+            //Console.WriteLine(num2);
+            //Console.WriteLine(num4);
+            //Console.WriteLine(let2);
+
+            #endregion
+
+        }
+    }
+}
