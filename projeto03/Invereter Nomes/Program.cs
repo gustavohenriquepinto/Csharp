324d3480bbdae274ee7d0f9bcabfa58dc2419015
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-20 13:46:32 -0300

    Entrada e saída de dados

---
+++ projeto03/Invereter Nomes/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invereter_Nomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Coletando os nomes
+            string nome1, nome2, nome3, nome4, aux;
+
+            Console.Write("Digite o nome 1: ");
+            nome1 = Console.ReadLine();
+
+            Console.Write("Digite o nome 2: ");
+            nome2 = Console.ReadLine();
+
+            Console.Write("Digite o nome 3: ");
+            nome3 = Console.ReadLine();
+
+            Console.Write("Digite o nome 4: ");
+            nome4 = Console.ReadLine();
+            #endregion
+
+            #region Invertendo as variáveis
+
+            aux = nome1;
+            nome1 = nome4;
+            nome4 = aux;
+
+            aux = nome2;
+            nome2 = nome3;
+            nome3 = aux;
+
+
+            #endregion
+
+            #region Exibindo os resultados invertidos
+
+            Console.WriteLine("\nNomes na ordem inversa:\n");
+            Console.WriteLine(nome1);
+            Console.WriteLine(nome2);
+            Console.WriteLine(nome3);
+            Console.WriteLine(nome4);
+
+            #endregion
+
+            #region Desinvertendo as variáveis
+
+            aux = nome1;
+            nome1 = nome4;
+            nome4 = aux;
+
+            aux = nome2;
+            nome2 = nome3;
+            nome3 = aux;
+
+            #endregion
+
+            #region Exibindo os resultados
+
+            Console.WriteLine("\nNomes na ordem certa:\n");
+            Console.WriteLine("\n" + nome1);
+            Console.WriteLine(nome2);
+            Console.WriteLine(nome3);
+            Console.WriteLine(nome4);
+
+            #endregion
+        }
+    }
+}
---
+++ projeto03/projeto03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Esse quebra a linha");
+            //Console.Write("Esse não vai quebrar a linha.");
+
+            //Console.Read();
+            //Console.ReadKey(); //Retorna o código da primeira letra em valor inteiro
+            Console.WriteLine("Qual é seu nome?");
+            string nome = Console.ReadLine(); //Retorna o texto digitado em string
+            Console.WriteLine("Olá, " + nome);
+            
+
+        }
+    }
+}
