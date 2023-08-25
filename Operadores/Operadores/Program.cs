6d1af8d39c8cf2a65ff23a0d6ef8960308bc0d91
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-24 22:23:09 -0300

    Operadores
    Operadores aritméticos

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores Aritméticos
+            //byte n1 = 10 + 2;
+            //sbyte n2 = 8 - 15;
+            //byte n3 = Convert.ToByte(n1 + n2);
+            //int n4 = n1 + n2;
+
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+
+            //double n1 = 2;
+            //double n2 = 3;
+
+            //Console.WriteLine(n1 + n2);
+            //Console.WriteLine(n1 - n2);
+            //Console.WriteLine(n1 * n2);
+            //Console.WriteLine(n1 / n2);
+            //Console.WriteLine(n1 % n2);
+
+            #endregion
+
+        }
+    }
+}
