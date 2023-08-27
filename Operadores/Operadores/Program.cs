18e8067c845bf4f6c85a6e3ffe93ef50e81ecc01
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-08-27 17:57:09 -0300

    operadores

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -34,22 +34,69 @@ namespace Operadores
 
             #region Precedência de Operadores
 
-            int n1, n2, n3, r1, r2, r3;
-            n1 = 100;
-            n2 = 10;
-            n3 = 5;
+            //int n1, n2, n3, r1, r2, r3;
+            //n1 = 100;
+            //n2 = 10;
+            //n3 = 5;
 
-            r1 = n1 + n2 * n3;
-            r2 = n1 - n2 / n3;
-            r3 = (n1 + n2) / n3;
-            double r4 = (double)n1 / 3 + n2 * n1;
+            //r1 = n1 + n2 * n3;
+            //r2 = n1 - n2 / n3;
+            //r3 = (n1 + n2) / n3;
+            //double r4 = (double)n1 / 3 + n2 * n1;
 
-            Console.WriteLine(r1);
-            Console.WriteLine(r2);
-            Console.WriteLine(r3);
-            Console.WriteLine(r4);
+            //Console.WriteLine(r1);
+            //Console.WriteLine(r2);
+            //Console.WriteLine(r3);
+            //Console.WriteLine(r4);
 
             #endregion
+
+            #region Operadores de Incremento e Decremento
+            //int n1 = 10;
+            //n1++;
+            //Console.WriteLine(n1);
+            //n1--;
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n1++);
+            //Console.WriteLine(--n1);
+            #endregion
+
+            #region Operador de Concatenação
+            //string nome1 = "Gustavo ";
+            //string nome2 = "Henrique";
+            //string NomeCompleto = nome1 + nome2;
+
+            //Console.WriteLine("Gustavo "+"Henrique");
+            //Console.WriteLine(NomeCompleto);
+            #endregion
+
+
+            //double n1, n2;
+            //n1 = 4;
+            //n2 = 7;
+
+            //n1 = n2;
+            //Console.WriteLine(n1);
+
+            //n1 = 4;
+            //n1 += n2;
+            //Console.WriteLine(n1);
+
+            //n1 = 4;
+            //n1 -= n2;
+            //Console.WriteLine(n1);
+
+            //n1 = 4;
+            //n1 *= n2;
+            //Console.WriteLine(n1);
+
+            //n1 = 4;
+            //n1 /= n2;
+            //Console.WriteLine(n1);
+
+            //n1 = 4;
+            //n1 %= n2;
+            //Console.WriteLine(n1);
         }
     }
-}
+} 
