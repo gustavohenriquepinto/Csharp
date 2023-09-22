e584604b1aeb0e40151132dc33d631e726d934d9
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-09-21 21:46:11 -0300

    Modificadores

---
+++ ClasseObjetos/Modificadores/Classes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores
+{
+    class Teste
+    {
+        public string nome;
+        private string sobrenome;
+
+        private void Metodo() { }
+        public void Executar() { }
+
+    }
+    class Humano
+    {
+        protected string nome;
+        private string sobrenome;
+    }
+    class Homem : Humano
+    {
+        public void Metodo() { 
+            nome="";
+        }
+    }
+}
---
+++ ClasseObjetos/Modificadores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+
+            t.nome = "";
+            //t.sobrenome = "";
+            //t.Metodo();
+            t.Executar();
+
+            Homem h = new Homem();
+            h.Metodo();
+
+        }
+    }
+}
