eddfe6c9b4a2a89e2f32ad7f5caffc973f92bb5b
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-09-27 17:15:46 -0300

    Recursividade, argumentos do método main

---
+++ Recursividade/Recursividade/Recursivo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class Recursivo
+    {
+        public void Executar(string mensagem, int numero)
+        {
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+        }
+
+        public void ExecutarRecursivo(string mensagem, int numero)
+        {
+            if (numero > 0)
+            {
+                Console.WriteLine(mensagem);
+                ExecutarRecursivo(mensagem, numero - 1);
+            }
+        }
+    }
+}
