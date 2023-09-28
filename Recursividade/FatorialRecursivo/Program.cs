4aa282a090dd6f3d68ede1ec485e78ebc334382f
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-09-28 19:51:43 -0300

    Fatorial Recursivo
    Exercício de programação para o cálculo de fatorial utilizando o conceito de recursividade.

---
+++ Recursividade/FatorialRecursivo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecursivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual fatorial você quer calcular? ");
+            int calcular = int.Parse(Console.ReadLine());
+
+            long fatorial = Fatorial(calcular);
+
+            if(fatorial == 0)
+            {
+                Console.WriteLine("\nVocê digitou um número inválido.");
+            }
+            else
+            {
+                Console.WriteLine("\nO resultado é: " + fatorial);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static long Fatorial(int numero)
+        {
+            if (numero > 1)
+            {
+                return numero * Fatorial(numero - 1);
+            }
+            else if (numero >= 0)
+            {
+                return 1;
+            }
+            return 0;
+            
+        }
+    }
+}
