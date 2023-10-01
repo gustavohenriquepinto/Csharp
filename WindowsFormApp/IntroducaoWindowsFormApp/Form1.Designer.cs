feaa2cd355183c4051e3c412b1c408774ba0010f
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-10-01 12:30:14 -0300

    Controlers Properties
    Font, size, enabled/disabled button

--- WindowsFormApp/IntroducaoWindowsFormApp/Form1.Designer.cs
+++ WindowsFormApp/IntroducaoWindowsFormApp/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.botao01 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // botao01
@@ -37,24 +38,41 @@
             this.botao01.BackColor = System.Drawing.Color.Black;
             this.botao01.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.botao01.ForeColor = System.Drawing.Color.Coral;
-            this.botao01.Location = new System.Drawing.Point(12, 12);
+            this.botao01.Location = new System.Drawing.Point(28, 48);
+            this.botao01.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.botao01.Name = "botao01";
-            this.botao01.Size = new System.Drawing.Size(246, 123);
+            this.botao01.Size = new System.Drawing.Size(156, 93);
             this.botao01.TabIndex = 0;
             this.botao01.Text = "Não Clique Aqui!";
             this.botao01.UseVisualStyleBackColor = false;
             this.botao01.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Texto aleatório";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
-            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.ClientSize = new System.Drawing.Size(864, 421);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.botao01);
+            this.Font = new System.Drawing.Font("MV Boli", 15F, ((System.Drawing.FontStyle)((((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(1000, 500);
-            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximumSize = new System.Drawing.Size(2148, 964);
+            this.MinimumSize = new System.Drawing.Size(54, 50);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Olá, Mundo!";
@@ -68,6 +86,7 @@
         #endregion
 
         private System.Windows.Forms.Button botao01;
+        private System.Windows.Forms.Label label1;
     }
 }
 
