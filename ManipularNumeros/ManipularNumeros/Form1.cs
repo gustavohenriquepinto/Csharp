304ac92447dd3d35f5c7428bbdbae1b4b3f711d1
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-10-25 17:46:02 -0300

    Classe Math

--- ManipularNumeros/ManipularNumeros/Form1.cs
+++ ManipularNumeros/ManipularNumeros/Form1.cs
@@ -20,17 +20,40 @@ namespace ManipularNumeros
 
         private void btnExecutar_Click(object sender, EventArgs e)
         {
-            //int num;
-            //bool res = int.TryParse("2004hjowrf", out num)
+            #region TryParse
+            ////int num;
+            ////bool res = int.TryParse("2004hjowrf", out num)
 
-            if (int.TryParse("2004", out int num))
-            {
-                lblResultado.Text = num.ToString();
-            }
-            else
-            {
-                lblResultado.Text = "Erro na conversão";
-            }
+            //if (int.TryParse("2004", out int num))
+            //{
+            //    lblResultado.Text = num.ToString();
+            //}
+            //else
+            //{
+            //    lblResultado.Text = "Erro na conversão";
+
+            #endregion
+
+            #region ToString
+            ////int num = 120;
+            ////lblResultado.Text = num.ToString("00000.00");
+
+            //int num = 14;
+            //lblResultado.Text = num.ToString("X"); //Hexadecimal
+            //lblResultado.Text = num.ToString("D"); //Decimal
+            //lblResultado.Text = num.ToString("C"); //Monetário
+            #endregion
+
+            #region Classe Math
+
+            double ctePi = Math.PI;
+            double cteE = Math.E;
+            double sen30 = Math.Sin(30);
+            double cos30 = Math.Cos(30);
+            double potencia = Math.Pow(2, 5);
+            lblResultado.Text = ctePi.ToString("0.00");
+
+            #endregion
         }
     }
 }
