dffcc8428b17fa13b4be0c84b4a95471da14c789
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2023-10-28 09:01:32 -0300

    Listas genéricas e seus métodos

--- Colecoes/Colecoes/Form1.cs
+++ Colecoes/Colecoes/Form1.cs
@@ -1,3 +1,5 @@
+using System.Xml.Linq;
+
 namespace Colecoes
 {
     public partial class Form1 : Form
@@ -9,6 +11,8 @@ namespace Colecoes
 
         private void btnList_Click(object sender, EventArgs e)
         {
+            lista.Items.Clear();
+
             //Array
             string[] users = new string[3];
             users[0] = "Gustavo";
@@ -22,11 +26,39 @@ namespace Colecoes
             usersList.Add("Davi");
             usersList.AddRange(users);
 
-            lista.Items.Add(usersList[0]);
-            lista.Items.Add(usersList[1]);
-            lista.Items.Add(usersList[2]);
-            lista.Items.Add(usersList[3]);
-            lista.Items.Add(usersList[4]);
+            //if (usersList.Remove(usersList[0]))
+            //{
+            //    MessageBox.Show("O usuário foi removido.");
+            //}
+            //else
+            //{
+            //    MessageBox.Show($"{usersList[0]} não pôde ser removido.");
+            //}
+
+            //if(usersList.Contains("Davi"))
+            //{
+            //    MessageBox.Show("Contém Davi.");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não contém Davi");
+            //}
+
+            //MessageBox.Show("Apresenta " + usersList.Count + " elementos.");
+            //usersList.Sort();
+
+            //MessageBox.Show("Davi está no índice: " + usersList.IndexOf("Davi"));
+
+            //usersList.Clear();
+
+            usersList.Insert(2, "João");
+            usersList.RemoveAt(5);
+
+            foreach(string name in usersList)
+            {
+                lista.Items.Add(name);
+            }
+            
         }
     }
 }
